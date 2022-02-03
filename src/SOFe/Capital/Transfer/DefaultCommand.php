4b41d2f704811bdf8d532ac9f84649ad14bdf246
DiamondStrider1 <62265561+Swift-Strider@users.noreply.github.com>
2022-02-03 02:14:27 -0600

    run `make fmt`

--- src/SOFe/Capital/Transfer/DefaultCommand.php
+++ src/SOFe/Capital/Transfer/DefaultCommand.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace SOFe\Capital\Transfer;
 
-use SOFe\Capital\ParameterizedLabelSet;
-
 class DefaultCommand {
     /**
      * @var array<string, string> $transactionLabels
