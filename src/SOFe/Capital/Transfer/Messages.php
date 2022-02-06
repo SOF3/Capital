357b8e23ff770426122d74d5327c746fc258f755
DiamondStrider1 <62265561+Swift-Strider@users.noreply.github.com>
2022-02-06 03:12:41 -0600

    chnage default messages given by Messages::parse()

--- src/SOFe/Capital/Transfer/Messages.php
+++ src/SOFe/Capital/Transfer/Messages.php
@@ -11,13 +11,13 @@ final class Messages {
     {
         return new self (
             $parser->expectString("player-only-command", $default?->playerOnlyCommand ?? "{red}Only players may use this command.", "Sent to command sender if this command requires them to be player and they are not."),
-            $parser->expectString("notify-sender-success", $default?->notifySenderSuccess ?? "{green}notify-sender-success (override me!)", "Sent to command sender on success."),
-            $parser->expectString("notify-recipient-success", $default?->notifyRecipientSuccess ?? "{green}notify-recipient-success (override me!)", "Sent to recipient on success."),
-            $parser->expectString("no-source-accounts", $default?->noSourceAccounts ?? "{red}no-recipient-accounts (override me!)", "Sent when no source accounts are found."),
-            $parser->expectString("no-destination-accounts", $default?->noDestinationAccounts ?? "{red}no-destination-accounts (override me!)", "Sent when no destination accounts are found."),
-            $parser->expectString("underflow", $default?->underflow ?? "{red}underflow (override me!)", "Sent when too much money is withdrawn."),
-            $parser->expectString("overflow", $default?->overflow ?? "{red}overflow (override me!)", "Sent when too much money is given."),
-            $parser->expectString("internal-error", $default?->internalError ?? "{red}internal-error (override me!)", "Sent when an unexpected error occurs."),
+            $parser->expectString("notify-sender-success", $default?->notifySenderSuccess ?? "{green}notify-sender-success (change this!)", "Sent to command sender on success."),
+            $parser->expectString("notify-recipient-success", $default?->notifyRecipientSuccess ?? "{green}notify-recipient-success (change this!)", "Sent to recipient on success."),
+            $parser->expectString("no-source-accounts", $default?->noSourceAccounts ?? "{red}no-recipient-accounts (change this!)", "Sent when no source accounts are found."),
+            $parser->expectString("no-destination-accounts", $default?->noDestinationAccounts ?? "{red}no-destination-accounts (change this!)", "Sent when no destination accounts are found."),
+            $parser->expectString("underflow", $default?->underflow ?? "{red}underflow (change this!)", "Sent when too much money is withdrawn."),
+            $parser->expectString("overflow", $default?->overflow ?? "{red}overflow (change this!)", "Sent when too much money is given."),
+            $parser->expectString("internal-error", $default?->internalError ?? "{red}internal-error (change this!)", "Sent when an unexpected error occurs."),
         );
     }
 
